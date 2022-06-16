4afcdfefb298ea37a437bc27ef830068b99dd9fc
HP <kunceviclera02gmail.com>
2022-06-16 16:03:48 +0300

    second commit

---
+++ src/scraping/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Vacancy
+
+
+
+def home_view(request):
+    qs = Vacancy.objects.all()
+    return render(request, 'home.html', {'object_list': qs})
